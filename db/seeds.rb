3ed135f84c3be307f70467a3ca681a4ddb89c7cb
Romain Jumeau <romain.jumeau@hotmail.fr>
2021-11-10 10:37:59 +0100

    update but not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  Article.create do |article|
+    article.title = Faker::Superhero.name
+    article.content = Faker::Quotes::Shakespeare.hamlet_quote
+  end
+end
